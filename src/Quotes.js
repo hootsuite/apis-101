94b88cceacdad590e3a58012ca43e492b82ebd04
Kush Patel <kush.patel@hootsuite.com>
2020-03-02 14:04:36 -0800

    solution including author

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,32 +1,18 @@
 import React, { useState } from 'react';
 
 function Quotes() {
-  // We are using a React feature called "Hooks"
-  // You can read more about React Hooks here https://reactjs.org/docs/hooks-intro.html
-  // Bonus: Can quote include author? Then display "Here is a default quote" -Author
-  const [quote, setQuote] = useState('Here is a default quote');
-
-  // Bonus: Get all quotes from an author (i.e Albert Einstein, Aristotle, Buddha) on click of button
+  const [quote, setQuote] = useState({ text: 'Here is a default quote', author: 'Anonymous' });
 
   function generateQuoteClicked() {
-    // This function will trigger every time the "Generate Quote" button is clicked
-    // Make your API request here to get a random quote: https://pprathameshmore.github.io/QuoteGarden/#get-a-random-quote
-    // Have a look at https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    // The section that reads "A basic fetch request is really simple to set up" will help you with this
-    fetch(/* TODO */)
+    fetch("https://quote-garden.herokuapp.com/quotes/random")
       .then(resp => resp.json())  
-      .then(resp => {
-          // resp is what we're getting back from the API call we make
-          // It should match the API Documentation we read 
-          // Take a look at the resp in your browser console, then use setQuote to set the quote
-          console.log(resp);
-      })
-      .catch(error => console.log('error: ', error)) // What would you show to the user in case of error?
+      .then(resp => setQuote({ text: resp.quoteText, author: resp.quoteAuthor }))
+      .catch(error => console.log('error: ', error))
   }
 
   return (
     <div className="Quotes">
-      <div className="Quote">"{quote}"</div>
+      <div className="Quote">"{quote.text}" -{quote.author}</div>
       <button onClick={generateQuoteClicked}>Generate Quote</button>
     </div>
   );
